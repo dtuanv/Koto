05b6ddf72ccf5f02fcefd0fb43a14febe6d5938d
dtuan <dtuanlieb@gmail.com>
2022-11-08 00:33:26 +0100

    api

--- src/apis/Api.js
+++ src/apis/Api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
   const Api = axios.create({
-    baseURL : process.env.DEV ? "http://localhost:8686" : "https://waxyrice.de/api"
+    baseURL : process.env.DEV ? "http://localhost:8686" : "https://koto-asiankitchen.de/api"
   })
   export default Api;
